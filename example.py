7a58762d1a8cb6fead527c78f864bdb22afdbf18
Veronica Agyapong <veronysuccess@gmail.com>
2024-06-24 05:26:13 +0000

    made a change

--- example.py
+++ example.py
@@ -1,10 +1,3 @@
-# import requests
-
-# url = 'https://httpbin.org/post'
-# response = requests.post(url)
-
-# print(response.json())
-
 import requests
 
 response = requests.get('https://api.coindesk.com/v1/bpi/currentprice/BTC.json')
